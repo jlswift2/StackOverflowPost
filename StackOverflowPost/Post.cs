78943092aa1692132ff32c160d65956ed2f1f80a
jlswift2 <jlswift2@gmail.com>
2022-06-07 01:30:14 -0500

    Add project files.

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        //Properties
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        private DateTime _created;
+        private int _count;
+        public int VoteCount
+        { 
+            get { return _count; }
+        }
+
+        //constructors 
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            _created = DateTime.Now;
+        }
+
+
+        // methods
+
+        public void UpVote()
+        {
+            _count++;
+        }
+
+        public void DownVote()
+        {
+            _count--;
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackOverflowPost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post("Title", "We are testing here.");
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Description);
+            Console.WriteLine($"The total number of likes is {post.VoteCount}");
+        }
+    }
+}
